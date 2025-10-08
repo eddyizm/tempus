45793c343aa768e1fc451be33fbe5c6fdbda340e
eddyizm <eddyizm@gmail.com>
2025-10-07 21:26:36 -0700

    chore: formatting

--- app/src/main/java/com/cappielloantonio/tempo/widget/WidgetProvider4x1.java
+++ app/src/main/java/com/cappielloantonio/tempo/widget/WidgetProvider4x1.java
@@ -4,5 +4,6 @@ package com.cappielloantonio.tempo.widget;
  * AppWidget provider entry for the 4x1 widget card. Inherits all behavior
  * from {@link WidgetProvider}.
  */
-public class WidgetProvider4x1 extends WidgetProvider {}
+public class WidgetProvider4x1 extends WidgetProvider {
+}
 
