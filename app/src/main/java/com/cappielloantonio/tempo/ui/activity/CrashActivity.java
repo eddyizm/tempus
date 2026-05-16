678e97e315e245284ee28609816b7e4e872756a2
Tom <tvillega@mailbox.org>
2026-05-16 11:56:04 -0400

    feat: add landscape layout to crash activity (#648)
    * feat: add drawer to landscape layout of crash activity
    and remove the bottom navbar on landscape as well
    
    * feat: add landscape layout to crash info fragment
    
    * feat: add landscape layout to crash export fragment
    
    * feat: add landscape layout to crash logs fragment
    
    * fix: toolbar not initiated nor configured
    
    ---------
    
    Co-authored-by: eddyizm <eddyizm@gmail.com>

--- app/src/main/java/com/cappielloantonio/tempo/ui/activity/CrashActivity.java
+++ app/src/main/java/com/cappielloantonio/tempo/ui/activity/CrashActivity.java
@@ -1,14 +1,17 @@
 package com.cappielloantonio.tempo.ui.activity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.splashscreen.SplashScreen;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.media3.common.util.UnstableApi;
 
@@ -19,6 +22,9 @@ import com.cappielloantonio.tempo.ui.fragment.CrashInfoFragment;
 import com.cappielloantonio.tempo.ui.fragment.CrashLogsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.color.DynamicColors;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 import cat.ereza.customactivityoncrash.CustomActivityOnCrash;
 import cat.ereza.customactivityoncrash.config.CaocConfig;
@@ -29,7 +35,11 @@ public class CrashActivity extends AppCompatActivity {
     ActivityCrashBinding bind;
     String stackTraceFromIntent;
     CaocConfig configFromIntent;
+    private Toolbar toolbar;
     private BottomNavigationView bottomNav;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +56,7 @@ public class CrashActivity extends AppCompatActivity {
         configFromIntent = CustomActivityOnCrash.getConfigFromIntent(getIntent());
 
         init();
+        initAppBar();
     }
 
     @Override
@@ -68,13 +79,40 @@ public class CrashActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_crash);
 
-        Toolbar toolbar = findViewById(R.id.crash_toolbar);
+        toolbar = findViewById(R.id.crash_toolbar);
+        setSupportActionBar(toolbar);
+
         FrameLayout contentFrame = findViewById(R.id.crash_content_frame);
 
+        // These will be null if the view is not present in the current layout
         bottomNav = findViewById(R.id.crash_bottom_nav);
+        drawerLayout = findViewById(R.id.crash_drawer_layout);
+        navView = findViewById(R.id.crash_nav_view);
 
         if (bottomNav != null) {
             setupBottomNav(bottomNav);
+        }
+
+        if (drawerLayout != null && navView != null) {
+            setupDrawer(drawerLayout, navView);
+        }
+    }
+
+    private void initAppBar() {
+
+        int orientation = getResources().getConfiguration().orientation;
+        boolean isLandscape = orientation == Configuration.ORIENTATION_LANDSCAPE;
+
+        if (isLandscape) {
+            ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(
+                    this,
+                    drawerLayout,
+                    toolbar,
+                    R.string.toolbar_navigation_drawer_open,
+                    R.string.toolbar_navigation_drawer_closed
+            );
+            drawerLayout.addDrawerListener(drawerToggle);
+            drawerToggle.syncState();
         }
     }
 
@@ -87,26 +125,46 @@ public class CrashActivity extends AppCompatActivity {
     }
 
     private void setupBottomNav(BottomNavigationView nav) {
+        String toolbarTitle = "Crash Landing";
         nav.setOnNavigationItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.crash_nav_info) {
-                loadFragment(new CrashInfoFragment());
+                loadFragment(new CrashInfoFragment(), toolbarTitle);
                 return true;
             } else if (itemId == R.id.crash_nav_logs) {
-                loadFragment(new CrashLogsFragment());
+                loadFragment(new CrashLogsFragment(), toolbarTitle);
                 return true;
             } else if (itemId == R.id.crash_nav_export) {
-                loadFragment(new CrashExportFragment());
+                loadFragment(new CrashExportFragment(), toolbarTitle);
                 return true;
             }
             return false;
         });
 
-        // nav.setSelectedItemId(R.id.crash_nav_info);
-        loadFragment(new CrashInfoFragment());
-        nav.getMenu().findItem(R.id.crash_nav_info).setChecked(true);}
+        loadFragment(new CrashInfoFragment(), toolbarTitle);
+        nav.getMenu().findItem(R.id.crash_nav_info).setChecked(true);
+    }
 
-    private void loadFragment(Fragment fragment) {
+    private void setupDrawer(DrawerLayout drawer, NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.crash_nav_info) {
+                loadFragment(new CrashInfoFragment(), getString(R.string.ca_info_title));
+            } else if (itemId == R.id.crash_nav_logs) {
+                loadFragment(new CrashLogsFragment(), getString(R.string.ca_logs_title));
+            } else if (itemId == R.id.crash_nav_export) {
+                loadFragment(new CrashExportFragment(), getString(R.string.ca_export_title));
+            }
+            drawer.closeDrawers();
+            return true;
+        });
+        loadFragment(new CrashInfoFragment(), getString(R.string.ca_info_title));
+        navigationView.setCheckedItem(R.id.crash_nav_info);
+    }
+
+    private void loadFragment(Fragment fragment, String toolbarTitle) {
+        Objects.requireNonNull(getSupportActionBar())
+                .setTitle(toolbarTitle);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.crash_content_frame, fragment)
