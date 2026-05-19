ab9cd63b4db817cc5eb7985125ef2ae8ef3981ce
MaFo-28 <matth_f@yahoo.fr>
2026-05-19 08:00:39 +0200

    refactor Android Auto's constants

---
+++ app/src/tempus/java/com/cappielloantonio/tempo/util/ConstantsAA.kt
@@ -0,0 +1,67 @@
+package com.cappielloantonio.tempo.util
+
+object ConstantsAA {
+    // Android Auto max items
+    const val MAX_ITEMS = 500
+    const val MAX_SHUFFLE_ITEMS = 100
+
+    // Android Auto displayed constants
+    const val NUMBER_OF_DISPLAYED_ALBUMS = 15
+    const val NUMBER_OF_DISPLAYED_PODCASTS = 100
+    const val NUMBER_OF_DISPLAYED_RECENT_TRACKS = 100
+
+    // Android Root function
+    const val ROOT_ID = "[rootID]"
+    // Android Auto functions
+    const val HOME_ID = "[homeID]"
+    const val LAST_PLAYED_ID = "[lastPlayedID]"
+    const val ALBUMS_ID = "[albumsID]"
+    const val ARTISTS_ID = "[artistsID]"
+    const val MOST_PLAYED_ID = "[mostPlayedID]"
+    const val PLAYLIST_ID = "[playlistID]"
+    const val PODCAST_ID = "[podcastID]"
+    const val RADIO_ID = "[radioID]"
+    const val RECENTLY_ADDED_ID = "[recentlyAddedID]"
+    const val MADE_FOR_YOU_ID = "[madeForYouID]"
+    const val STARRED_BUNDLE_ID = "[starredBundleID]"
+    const val STARRED_TRACKS_ID = "[starredTracksID]"
+    const val STARRED_ALBUMS_ID = "[starredAlbumsID]"
+    const val STARRED_ARTISTS_ID = "[starredArtistsID]"
+    const val RANDOM_ID = "[randomID]"
+    const val FOLDER_ID = "[folderID]"
+    const val GENRES_ID = "[genresID]"
+    const val TRACKS_ID = "[TracksID]"
+    const val RECENT_TRACKS_ID = "[recentTracksID]"
+
+    // Android Auto System functions
+    const val INDEX_ID = "[indexID]"
+    const val DIRECTORY_ID = "[directoryID]"
+    const val ALBUM_ID = "[albumID]"
+    const val ARTIST_ID = "[artistID]"
+    const val JUMP_TO_ALBUMS_ID = "[jumpToAlbumsID]"
+    const val JUMP_TO_STARRED_ALBUMS_ID = "[jumpToStarredAlbumsID]"
+    const val JUMP_TO_ARTISTS_ID = "[jumpToArtistsID]"
+    const val JUMP_TO_STARRED_ARTISTS_ID = "[jumpToStarredArtistsID]"
+    const val ARTISTS_BY_ALBUMS_ID = "[artistsByAlbumsID]"
+    const val QUICKMIX_ID = "[quickmixID]"
+    const val MYMIX_ID = "[mymixID]"
+    const val DISCOVERYMIX_ID = "[discoverymixID]"
+
+    // Android Auto Source tag
+    const val QUEUE_CACHED_SOURCE = "[aaQueueCachedSource]"
+    const val INSTANTMIX_SOURCE = "[instantMixSource]"
+    const val MADE_FOR_YOU_SOURCE = "[madeForYouSource]"
+
+    // Android Auto InstantMix constants
+    const val INSTANT_MIX_MAX_TRACKS = 20
+    const val MIN_TRACKS_SMALL_MIX = INSTANT_MIX_MAX_TRACKS
+    const val MIN_TRACKS_MEDIUM_MIX = INSTANT_MIX_MAX_TRACKS+10
+    const val MIN_TRACKS_LARGE_MIX = INSTANT_MIX_MAX_TRACKS+20
+    const val NUMBER_OF_TRACKS_IN_SMALL_MIX = 12
+    const val NUMBER_OF_TRACKS_IN_MEDIUM_MIX = 15
+    const val NUMBER_OF_TRACKS_IN_LARGE_MIX = 18
+
+    // Android Auto MadeForYou constants
+    const val NUMBER_OF_RECENT_ALBUMS_FOR_MIX = 20
+    const val NUMBER_OF_RECENT_TRACKS_FOR_MIX = 50
+}
