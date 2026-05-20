967d93e25e8252d5a1b72c3f619da00889f041c9
Tomás Villegas <tvillega@mailbox.org>
2026-05-20 13:50:04 -0400

    fix: merge conflicts

--- app/src/main/java/com/cappielloantonio/tempo/ui/fragment/EqualizerFragment.kt
+++ app/src/main/java/com/cappielloantonio/tempo/ui/fragment/EqualizerFragment.kt
@@ -20,7 +20,6 @@ import androidx.media3.common.util.UnstableApi
 import com.cappielloantonio.tempo.R
 import com.cappielloantonio.tempo.equalizer.EqualizerManager
 import com.cappielloantonio.tempo.service.BaseMediaService
-import com.cappielloantonio.tempo.service.EqualizerManager
 import com.cappielloantonio.tempo.service.MediaService
 import com.cappielloantonio.tempo.ui.activity.MainActivity
 import com.cappielloantonio.tempo.util.Preferences
