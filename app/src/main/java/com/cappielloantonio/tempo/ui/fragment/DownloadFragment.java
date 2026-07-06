4a35928199cafb99932ce131960a5f56ef20aea0
eddyizm <eddyizm@gmail.com>
2026-07-05 20:48:37 -0700

    refactor(DownloadFragment): add stream import for filtering

--- app/src/main/java/com/cappielloantonio/tempo/ui/fragment/DownloadFragment.java
+++ app/src/main/java/com/cappielloantonio/tempo/ui/fragment/DownloadFragment.java
@@ -44,6 +44,7 @@ import java.io.File;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @UnstableApi
 public class DownloadFragment extends Fragment implements ClickCallback {
